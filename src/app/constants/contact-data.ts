ef596c156e49c686769e8216e4d30c663a983c99
VeyselKaraali <134809294+VeyselKaraali@users.noreply.github.com>
2025-05-27 11:48:48 +0200

    Update imprint

--- src/app/constants/contact-data.ts
+++ src/app/constants/contact-data.ts
@@ -1,7 +1,8 @@
 export const CONTACT_DATA = {
   name: 'Veysel Karaali',
-  street: '***REMOVED***',
-  city: '***REMOVED***',
+  company: 'c/o Block Services',
+  street: 'Stuttgarter Str. 106',
+  city: '70736 Fellbach',
   phone: '***REMOVED***',
   email: '***REMOVED***'
 };
